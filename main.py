1e72894f7e996f0fb4e5833c0615b98cf59c026f
CYRIL-NJITARI10 <cyrilnjitari7@gmail.com>
2023-09-24 12:03:00 +0200

    requirement

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async 
-src="https://www.googletagmanager.com/gtag/js?id=G-VXZ6YKPHXC"></script>
+src="https://www.googletagmanager.com/gtag/js?id=G-YGLCEC3NRQ"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', ' G-VXZ6YKPHXC');
+ gtag('config', ' G-YGLCEC3NRQ');
 </script>
  """
  return prefix_google + "Hello World"
