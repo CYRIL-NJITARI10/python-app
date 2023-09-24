5dfa0bd545fc40d2d46ac2047c082be4341311c7
CYRIL-NJITARI10 <cyrilnjitari7@gmail.com>
2023-09-24 10:56:58 +0200

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from flask import Flask
 
+def hello_world():
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async 
+src="https://www.googletagmanager.com/gtag/js?id=G-VXZ6YKPHXC"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' G-VXZ6YKPHXC');
+</script>
+ """
+ return prefix_google + "Hello World"
+
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return "Hello from Space! 🚀"
+
