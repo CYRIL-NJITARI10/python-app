c4411ff665d42934a0779ab9e4a2d8aebca54d77
CYRIL-NJITARI10 <cyrilnjitari7@gmail.com>
2023-09-24 13:29:40 +0200

    Mise à jour de main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask
 
+...
 def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async 
-src="https://www.googletagmanager.com/gtag/js?id=G-YGLCEC3NRQ"></script>
+src="https://www.googletagmanager.com/gtag/js?id=G-RW8X8N6FVS"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', ' G-YGLCEC3NRQ');
+ gtag('config', ' G-RW8X8N6FVS');
 </script>
  """
  return prefix_google + "Hello World"
